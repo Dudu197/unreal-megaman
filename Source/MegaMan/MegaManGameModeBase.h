fd2b0adbf95667bdb55b06efa799d6d29d14960a
Carlos Eduardo <cadu97@gmail.com>
2020-11-02 15:17:49 -0300

    Initial commit

---
+++ Source/MegaMan/MegaManGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MegaManGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEGAMAN_API AMegaManGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
